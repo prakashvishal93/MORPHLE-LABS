68099aec0ac0cf63062f74f910cd8f92110c37cd
Vishal Prakash <85199417+prakashvishal93@users.noreply.github.com>
2025-03-07 09:43:34 +0000

    Initial commit - Flask HTOP app

---
+++ flask-htop/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+import os
+import subprocess
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/htop')
+def htop():
+    # Your full name
+    name = "Vishal Prakash" 
+    
+
+    username = os.getenv("USER") or os.getenv("USERNAME") or "codespace"
+
+
+    ist_time = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30)
+    formatted_time = ist_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+
+    top_output = subprocess.getoutput("top -l 1")
+
+
+    response = f"""
+    <pre>
+    Name: {name}
+    User: {username}
+    Server Time (IST): {formatted_time}
+    
+    TOP output:
+    {top_output}
+    </pre>
+    """
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
