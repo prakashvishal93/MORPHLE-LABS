d9207ad95c72ed9be0f302f092009456d251a0b3
Vishal Prakash <85199417+prakashvishal93@users.noreply.github.com>
2025-03-07 10:25:11 +0000

    fix the error in the code

--- flask-htop/app.py
+++ flask-htop/app.py
@@ -7,20 +7,23 @@ app = Flask(__name__)
 
 @app.route('/htop')
 def htop():
-    # Your full name
+
     name = "Vishal Prakash"
 
-    # Get the system username
+
     username = os.getenv("USER") or os.getenv("USERNAME") or "codespace"
 
-    # Get current IST time
+
     ist_time = datetime.datetime.utcnow() + datetime.timedelta(hours=5, minutes=30)
     formatted_time = ist_time.strftime("%Y-%m-%d %H:%M:%S.%f")
 
-    # Use the correct `top` command for Linux
-    top_output = subprocess.getoutput("top -b -n 1")
 
-    # Format the response as HTML
+    if os.uname().sysname == "Darwin":  
+        top_output = subprocess.getoutput("top -l 1")
+    else:  # Linux
+        top_output = subprocess.getoutput("top -b -n 1")
+
+
     response = f"""
     <pre>
     Name: {name}
